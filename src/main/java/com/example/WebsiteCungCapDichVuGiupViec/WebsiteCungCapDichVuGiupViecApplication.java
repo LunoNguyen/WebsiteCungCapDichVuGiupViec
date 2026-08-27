92db10b90eb34a686a2e693ba1f3b3c6fee2d480
LunoNguyen <nnl8a1@gmail.com>
2026-08-28 02:22:57 +0700

    Initialize project

---
+++ src/main/java/com/example/WebsiteCungCapDichVuGiupViec/WebsiteCungCapDichVuGiupViecApplication.java
@@ -0,0 +1,13 @@
+package com.example.WebsiteCungCapDichVuGiupViec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteCungCapDichVuGiupViecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsiteCungCapDichVuGiupViecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/WebsiteCungCapDichVuGiupViec/WebsiteCungCapDichVuGiupViecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WebsiteCungCapDichVuGiupViec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteCungCapDichVuGiupViecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
